688ec907bb7737bad5e4fe7619012ad7570573bd
ex-zhujw002 <ex-zhujw002@cmft.com>
2020-01-03 17:36:32 +0800

    角色权限

---
+++ src/views/knowledge/role/DataAuthority.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="data-authority">
+    <div class="data-authority-side">
+      <!-- <div class="data-authority-side_title">用户<Button>新增</Button></div> -->
+      <List border>
+        <div class="data-authority-side_title" slot="header">
+          用户<Button @click="showAddModal">新增</Button>
+        </div>
+        <ListItem>
+          <span class="phone">1881239302</span>
+          <span class="name">雏鸡</span>
+        </ListItem>
+        <ListItem>This is a piece of text.</ListItem>
+        <ListItem>This is a piece of text.</ListItem>
+      </List>
+    </div>
+    <div class="data-authority-content">
+      <div class="title">
+        数据权限
+        <span class="tips">有数据的用户才具有角色权限的浏览权限、操作权限</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataAuthority",
+
+  data() {
+    return {
+      addModal: false
+    };
+  },
+
+  methods: {
+    showAddModal() {
+      this.addModal = true;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.data-authority {
+  padding: 2%;
+  background: #ffffff;
+  overflow: hidden;
+  &-side {
+    float: left;
+  }
+  &-content {
+    float: left;
+  }
+}
+</style>
--- src/views/knowledge/role/index.vue
+++ src/views/knowledge/role/index.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>
-    角色权限
+  <div class="authority-warp">
+    <Tabs type="card">
+      <TabPane label="数据权限">
+        <DataAuthority></DataAuthority>
+      </TabPane>
+      <TabPane label="账号角色">标签二的内容</TabPane>
+      <TabPane label="角色权限">标签二的内容</TabPane>
+      <TabPane label="权限树">标签三的内容</TabPane>
+    </Tabs>
   </div>
 </template>
 
 <script>
+import DataAuthority from "./DataAuthority.vue";
 export default {
-  name: "Role"
+  name: "Role",
+
+  components: {
+    DataAuthority
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.authority-warp {
+  padding: 0 10px;
+}
+</style>
